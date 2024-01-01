3946726f3497901e6b83d0758a310c906f48823e
Dwoyng <155347972+Dwoyng@users.noreply.github.com>
2024-01-01 15:16:49 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function login() {
     // (Có thể sử dụng AJAX để gửi thông tin đăng nhập đến máy chủ và kiểm tra bằng server-side code)
 
     // Ví dụ đơn giản: Kiểm tra nếu username là "admin" và password là "password"
-    if (username === "Trang" ) {
+    if (username === "Khánh" ) {
         window.location.href = "https://dwoyng.github.io/Project-demo/";
     } else {
         alert("Đăng nhập thất bại. Vui lòng kiểm tra lại tài khoản và mật khẩu.");
