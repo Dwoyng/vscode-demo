f5d81b3a796de4b736ee8ab3f6fb8916b896aa41
Dwoyng <155347972+Dwoyng@users.noreply.github.com>
2024-01-02 10:26:38 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function login() {
-    var username = document.getElementById("username").value;
-   
 
+function login() {
+    var c1 = document.getElementById("c1").value;
+    var c2 = document.getElementById("c2").value;
+    var c3 = document.getElementById("c3").value;
     // Thực hiện kiểm tra tài khoản và mật khẩu ở đây
     // (Có thể sử dụng AJAX để gửi thông tin đăng nhập đến máy chủ và kiểm tra bằng server-side code)
 
     // Ví dụ đơn giản: Kiểm tra nếu username là "admin" và password là "password"
-    if (username === "Khánh" ) {
+    if ((c1 === "Khánh"||c1==="anh Khánh"||c1==="Hồ Quốc Khánh") && c2==="Trang"&& c3==="Chíp" ) {
         window.location.href = "https://dwoyng.github.io/Project-demo/";
     } else {
-        alert("Đăng nhập thất bại. Vui lòng kiểm tra lại tài khoản và mật khẩu.");
+        alert("Làm lại đi sai hết rồi :)))");
     }
 }
 
